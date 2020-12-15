c842d9cac238d38b531c7cde63c80af14bf1918f
Rifaldi Ardan <efalardan2020@gmail.com>
2020-12-15 21:23:49 +0700

    OOP DASAR pada PHP #3 - Property dan Method

--- index.php
+++ index.php
@@ -1,7 +1,16 @@
 <?php
+
 class Produk
 {
 
+    public $judul = "Judul", $penulis = "Penulis", $harga = "harga", $penerbit = "penerbit";
+
+    public function get_label()
+    {
+        return $this->judul.' '.$this->penerbit;
+    }
 }
 $produk1 = new Produk();
-$produk2 = new Produk();
+$produk1->judul = 'Naruto';
+print_r($produk1);
+print 'Label '.$produk1->get_label();
